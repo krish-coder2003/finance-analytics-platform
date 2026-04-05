1bd61c92e1aff5cc8dc4ecdf3be923dbee8f5fac
krish-coder2003 <krishnashrangare@gmail.com>
2026-04-05 17:06:40 +0530

    correct server.js

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./config/db');
 
-// Connect to database
-connectDB();
+// Connect DB and then start server
+connectDB().then(() => {
+  const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000;
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
 
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
-
-process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
+  process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
   });
 });
